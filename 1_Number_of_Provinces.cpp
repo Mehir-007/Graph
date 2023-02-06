61994c8918e1ec408b62bfd3de8564dd6ec28369
Mehir_007 <mehirrouth20000@gmail.com>
2023-02-06 16:28:38 +0530

    GRAPH

--- 1_Number_of_Provinces.cpp
+++ 1_Number_of_Provinces.cpp
@@ -1,7 +1,47 @@
+/*LEETCODE 547*/
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    void DFS(int node,vector<int> adj[],vector<int> &visited)
+    {
+        visited[node]=1;
+        for(auto x:adj[node])
+        {
+            if(visited[x]==0)
+            DFS(x,adj,visited);
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<int>adj[n];
+        vector<int> visited(n);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(isConnected[i][j]==1 && i!=j)
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i]!=1)
+            {
+                ans++;
+                DFS(i,adj,visited);
+            }
+        }
+        return ans;
+    }
+};
+
 int main()
 {
-    
+
 }
